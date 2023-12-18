ca3cd97d80173bce7fe2a8d2a27cb832249c7bd7
OFB <ofb_bbx@proton.me>
2023-12-18 22:55:56 +0100

    updated compte.js format

--- Commands/Economy/compte.js
+++ Commands/Economy/compte.js
@@ -18,77 +18,77 @@ module.exports = {
 
     if (!userEconomyRecord)
       return interaction.reply({
-        content: `Veuillez vous créer un compte bancaire avec la commande /nouveaucompte`,
+        content: `Veuillez vous créer un compte bancaire avec la commande </nouveaucompte:1186231398156222566>.`,
         ephemeral: true,
       });
 
     if (userEconomyRecord) {
       const res = new EmbedBuilder()
-      .setTitle("**Mon compte**")
-      .addFields(
-        {
-          name: "👤 **Prénom et Nom :**",
-          value: `${userEconomyRecord.names}`,
-        },
-        {
-          name: "\u200a",
-          value: "\u200a",
-        },
-        {
-          name: "👶 **Date de naissance :**",
-          value: `${userEconomyRecord.dateOfBirth}`,
-        },
-        {
-          name: "\u200a",
-          value: "\u200a",
-        },
-        {
-          name: "👤 **Sexe :**",
-          value: `${userEconomyRecord.gender}`,
-        },
-        {
-          name: "\u200a",
-          value: "\u200a",
-        },
-        {
-          name: "🏦 **Banque : **",
-          value: "Zirexium Finance",
-        },
-        {
-          name: "\u200a",
-          value: "\u200a",
-        },
-        {
-          name: "💸 **Solde: **",
-          value: `${userEconomyRecord.balance}`,
-        },
-        {
-          name: "\u200a",
-          value: "\u200a",
-        },
-        {
-          name: "💳 **Numéro de carte bancaire :**",
-          value:`${userEconomyRecord.creditCardNumber}`,
-        },
-        {
-          name: "\u200a",
-          value: "\u200a",
-        },
-        {
-          name: "💳 **Cryptogramme :**",
-          value: `${userEconomyRecord.cvc}`,
-        },
-        {
-          name: "\u200a",
-          value: "\u200a",
-        },
-        {
-          name: "💳** Date d'expiration :**",
-          value: `${userEconomyRecord.expirationDate}`,
-        }
-      )
-      .setTimestamp()
-      .setThumbnail(interaction.user.displayAvatarURL());
+        .setTitle("**Mon compte**")
+        .addFields(
+          {
+            name: "👤 **Prénom et Nom :**",
+            value: `${userEconomyRecord.names}`,
+          },
+          {
+            name: "\u200a",
+            value: "\u200a",
+          },
+          {
+            name: "👶 **Date de naissance :**",
+            value: `${userEconomyRecord.dateOfBirth}`,
+          },
+          {
+            name: "\u200a",
+            value: "\u200a",
+          },
+          {
+            name: "👤 **Sexe :**",
+            value: `${userEconomyRecord.gender}`,
+          },
+          {
+            name: "\u200a",
+            value: "\u200a",
+          },
+          {
+            name: "🏦 **Banque : **",
+            value: "Zirexium Finance",
+          },
+          {
+            name: "\u200a",
+            value: "\u200a",
+          },
+          {
+            name: "💸 **Solde: **",
+            value: `${userEconomyRecord.balance}`,
+          },
+          {
+            name: "\u200a",
+            value: "\u200a",
+          },
+          {
+            name: "💳 **Numéro de carte bancaire :**",
+            value: `${userEconomyRecord.creditCardNumber}`,
+          },
+          {
+            name: "\u200a",
+            value: "\u200a",
+          },
+          {
+            name: "💳 **Cryptogramme :**",
+            value: `${userEconomyRecord.cvc}`,
+          },
+          {
+            name: "\u200a",
+            value: "\u200a",
+          },
+          {
+            name: "💳** Date d'expiration :**",
+            value: `${userEconomyRecord.expirationDate}`,
+          }
+        )
+        .setTimestamp()
+        .setThumbnail(interaction.user.displayAvatarURL());
       interaction.reply({
         embeds: [res],
         ephemeral: true,
