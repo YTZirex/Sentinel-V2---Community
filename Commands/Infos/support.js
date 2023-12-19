8873e98e9c3bac0cbf5dc1a28f68a32b06838d7e
OFB <ofb_bbx@proton.me>
2023-12-19 13:58:07 +0100

    uhuiui

--- Commands/Infos/support.js
+++ Commands/Infos/support.js
@@ -10,7 +10,7 @@ module.exports = {
     .setDescription("Permet d'avoir le lien d'invitation de notre Support."),
   async execute(interaction) {
     interaction.reply({
-      content: `Vous pouvez rejoindre notre Support en cliquant [ici](https://discord.gg/My2BVCmJEY).`,
+      content: `https://discord.gg/My2BVCmJEY`,
       ephemeral: true,
     });
   },
