3fd44f7cce07f5cfc172a50fe533c77d3d770f1c
OFB <ofb_bbx@proton.me>
2023-12-19 13:51:21 +0100

    support command

---
+++ Commands/Infos/support.js
@@ -0,0 +1,17 @@
+const {
+  SlashCommandBuilder,
+  EmbedBuilder,
+  CommandInteraction,
+} = require("discord.js");
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("support")
+    .setDescription("Permet d'avoir le lien d'invitation de notre Support."),
+  async execute(interaction) {
+    interaction.reply({
+      content: `Vous pouvez rejoindre notre Support en cliquant <[ici](https://discord.gg/My2BVCmJEY)>`,
+      ephemeral: true,
+    });
+  },
+};
