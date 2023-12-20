f874847d72f323e981aabc1cbfa28621dd9f6d07
YTZirex <90211090+YTZirex@users.noreply.github.com>
2023-12-20 17:03:31 +0000

    guildcreate

---
+++ Events/Guild/guildCreate.js
@@ -0,0 +1,31 @@
+const { models, Schema } = require("mongoose");
+const welcomeSchema = require("../../Models/Welcome");
+
+module.exports = {
+  name: "guildCreate",
+  async execute(guild) {
+    const guildRecord = await welcomeSchema.findOne({
+      guild: guild.id,
+    });
+
+    if (!guildRecord) {
+      const newGuildRecord = new welcomeSchema({
+        guild: guild.id,
+        channel: "none",
+        message: "none",
+        role: "none",
+      });
+      await newGuildRecord.save();
+      return;
+    }
+
+    if (guildRecord) {
+      guildRecord.guild = guild.id;
+      guildRecord.channel = "none";
+      guildRecord.message = "none";
+      guildRecord.role = "none";
+      await guildRecord.save();
+      return;
+    }
+  },
+};
